dacc77c1e961c4722a7cb414a4b0271337683213
Viktoria <viki.boss10@gmail.com>
2024-05-08 14:56:40 +0300

    add full js commit

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ const makeOrderFormBackdrop = document.querySelector(".backdrop");
 const footerForm = document.querySelector(".footer-form");
 const footerFormSubmitButton = document.querySelector(".footer-form-button");
 
+const successfulOrderModal = document.querySelector(".backdrop-order-success");
+const successfulSubscribeModal = document.querySelector(
+  ".backdrop-footer-success"
+);
+
 openMobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.add("is-open");
 });
@@ -38,9 +43,52 @@ closeOrderFormButton.addEventListener("click", () => {
   makeOrderFormBackdrop.classList.remove("is-open");
 });
 
-// makeOrderForm.addEventListener("submit", () => {});
+makeOrderForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  successfulOrderModal.classList.add("is-open");
+  makeOrderForm.reset();
+});
+
 footerForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("thank you for filling this form!");
-  
+  successfulSubscribeModal.classList.add("is-open");
+  footerForm.reset();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (
+    !successfulOrderModal.classList.contains("is-open") &&
+    !successfulSubscribeModal.classList.contains("is-open")
+  ) {
+    return;
+  }
+
+  if (event.key === "Escape") {
+    if (successfulOrderModal.classList.contains("is-open")) {
+      successfulOrderModal.classList.remove("is-open");
+      makeOrderFormBackdrop.classList.remove("is-open");
+    }
+
+    if (successfulSubscribeModal.classList.contains("is-open")) {
+      successfulSubscribeModal.classList.remove("is-open");
+    }
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target !== successfulOrderModal &&
+    event.target !== successfulSubscribeModal
+  ) {
+    return;
+  }
+
+  if (successfulOrderModal.classList.contains("is-open")) {
+    successfulOrderModal.classList.remove("is-open");
+    makeOrderFormBackdrop.classList.remove("is-open");
+  }
+
+  if (successfulSubscribeModal.classList.contains("is-open")) {
+    successfulSubscribeModal.classList.remove("is-open");
+  }
 });
